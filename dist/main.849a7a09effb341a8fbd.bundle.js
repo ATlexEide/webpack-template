d02fdf6be8afaea7b21aa92a1471870c23d89453
Alexander Eide <atlexeide@gmail.com>
2024-10-07 00:12:24 +0200

    Update config

--- dist/main.849a7a09effb341a8fbd.bundle.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log("Hello World!");
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi44NDlhN2EwOWVmZmIzNDFhOGZiZC5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxjQUFjLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL3dlYnBhY2stdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZCFcIik7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ const config = {
     devServer: {
         open: true,
         host: 'localhost',
+        hot: false,
         liveReload: true,
     },
     plugins: [
